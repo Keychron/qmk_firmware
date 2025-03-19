e7762915de11941f323267145d282590a62ffa5e
Hiromi Watanabe <watanabe@mdk24.jp>
2025-03-19 09:03:04 +0900

    Updated rules.mk for keychron/k3_pro/jis/rgb

--- keyboards/keychron/k3_pro/jis/rgb/rules.mk
+++ keyboards/keychron/k3_pro/jis/rgb/rules.mk
@@ -1,1 +1,17 @@
-# This file intentionally blank
+# MCU & USB settings
+MCU = STM32L432KB
+BOOTLOADER = stm32-dfu
+
+# Enable features
+VIA_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
+
+# Optimization
+LTO_ENABLE = yes
+
+# Fix for build error
+SRC += matrix.c
+SRC += rgb.c
+
+
+include ../../rules.mk
